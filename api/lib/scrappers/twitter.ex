c2c93ae541224082f02fcaf41e113e3d161b7ec0
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-06 00:57:04 +0100

    tweet library done

--- api/lib/scrappers/twitter.ex
+++ api/lib/scrappers/twitter.ex
@@ -1,29 +1,61 @@
 defmodule FeedApi.Scrappers.Twitter do
 
-  @tweets 10
+  @tweets 1
 
   @doc """
   Some comments or something
   """
   def dispatch! do
-    ExTwitter.user_timeline(count: @tweets) |> stream_catch
+    ExTwitter.user_timeline(count: @tweets) |> fetch
   end
 
-  defp stream_catch(stream) do
+  @doc """
+  We fetch the stream and return all the datas we need
+  Which are the message, link and date of creation of the Tweet
+  """
+  defp fetch(stream) do
     for data <- stream do
-      handle(data) |> format
+      IO.inspect handle(data)
     end
   end
 
-  defp _parse(term) when is_atom(term), do: term
-
+  @doc """
+  Format the Tweet by removing the URL and triming it
+  """
   defp format(string) do
-    IO.inspect "hey #{string}"
+    String.replace(string, ~r/(?:f|ht)tps?:\/[^\s]+/, "") |> String.trim
   end
 
-  # defp handle(data = %ExTwitter.Model.Tweet), do: data.text
+  @doc """
+  Get only the data needed in our system from the Tweet
+  """
+  defp handle(data = %ExTwitter.Model.Tweet{}) do
+    IO.inspect format(data.text)
+    %{
+      message: format(data.text),
+      link: List.first(data.entities.urls).expanded_url,
+      date: handle(data.user).created_at
+    }
+  end
 
-  defp handle(data = %ExTwitter.Model.Tweet{}), do: data.text
-  defp handle(_), do: nil
+  @doc """
+  Get only the data needed in our system from the User
+  """
+  defp handle(data = %ExTwitter.Model.User{}) do
+    %{
+      created_at: data.created_at
+    }
+  end
+
+  @doc """
+  Null Object Pattern
+  """
+  defp handle(_) do
+    %{
+      tweet: nil,
+      link: nil,
+      date: nil
+    }
+  end
 
 end
