c1020f361527c0c832c869f39bb7ed5c9891a88c
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-06 14:56:58 +0100

    some comments added for clarity

--- app/app/controllers/links/feed.js
+++ app/app/controllers/links/feed.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    /**
+     * We refresh the links from the database by feeding on the back-end
+     */
     refresh: function() {
       Ember.$.ajax({
         url: '/api/links/feed',
--- app/app/routes/links/feed.js
+++ app/app/routes/links/feed.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel() {
+    /**
+     * We basically call the API via the controller method `refresh`
+     * And redirect instantly to the links
+     */
     this.controllerFor('links/feed').send('refresh');
     this.transitionTo('links');
   }
