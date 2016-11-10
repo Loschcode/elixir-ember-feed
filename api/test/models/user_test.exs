51c6732b3c3a101543339f25a0f39654b5e109c9
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-10 22:43:42 +0100

    authentication system progress

--- api/test/models/user_test.exs
+++ api/test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule FeedApi.UserTest do
 
   alias FeedApi.User
 
-  @valid_attrs %{email: "some content", password_hash: "some content"}
+  @valid_attrs %{email: "mike@example.com", password: "abcde12345", password_confirmation: "abcde12345"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -15,4 +15,25 @@ defmodule FeedApi.UserTest do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "mis-matched password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2bj1rjbk2",
+      password_confirmation: "b1bk23jkn12"})
+    refute changeset.valid?
+  end
+
+  test "missing password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2bj1rjbk2"})
+    refute changeset.valid?
+  end
+
+  test "short password doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2d",
+      password_confirmation: "1lh2d"})
+    refute changeset.valid?
+  end
+
 end
