b4fc1e3e3407f5bee4c4f4ebba79cc586feb2a9a
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-13 22:57:29 +0100

    bower component cleaned

--- app/ember-cli-build.js
+++ app/ember-cli-build.js
@@ -4,7 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
     sassOptions: {
       extension: 'sass'
     }
@@ -22,6 +21,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  //
+  app.import('bower_components/gridle/css/grid.css');
+  app.import('bower_components/gridle/js/gridle-full.min.js');
+  app.import('bower_components/moment/moment.js');
 
   return app.toTree();
 };
