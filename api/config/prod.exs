be2fd68e0a0a148701f42b6de285111423dabc12
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-10 15:59:06 +0100

    changing to dokku environment

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -7,13 +7,17 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :feed_api, FeedApi.Endpoint,
-  http: [port: 4000],
-  debug_errors: true,
-  server: true,
-  code_reloader: false,
-  cache_static_lookup: false,
-  check_origin: false,
-  watchers: []
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("HOSTNAME"), port: 80]
+
+# config :feed_api, FeedApi.Endpoint,
+#   http: [port: 4000],
+#   debug_errors: true,
+#   server: true,
+#   code_reloader: false,
+#   cache_static_lookup: false,
+#   check_origin: false,
+#   watchers: []
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
@@ -23,13 +27,13 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :feed_api, FeedApi.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "feed",
-  password: "feeding",
-  database: "feed",
-  hostname: "localhost",
-  pool_size: 10
+# config :feed_api, FeedApi.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "feed",
+#   password: "feeding",
+#   database: "feed",
+#   hostname: "localhost",
+#   pool_size: 10
 
 # Configures Twitter scrapper
 config :extwitter, :oauth, [
